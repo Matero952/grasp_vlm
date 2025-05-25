c90f7d679e40a009a8d2eca2dae63fe1c83fb960
Matero952 <mateofeit@gmail.com>
2025-05-25 16:07:23 -0600

    fixed up gemini a little bit

--- src/agents/gemini.py
+++ src/agents/gemini.py
@@ -13,12 +13,4 @@ class GeminiExperiment:
             model=self.model,
             contents = [img, self.prompt]
         )
-        result = (response.text).lower()
-        grasp = re.search(r"palmar pinch|power disk|precision disk|power sphere|precision sphere|sphere 3 finger|sphere 4 finger|tripod|inferior pincer|quadpod|lateral|extension type|palmar", result)
-        return response.text, grasp
-if __name__ == "__main__":
-    # print(vars(genai))
-    gemexperiment = GeminiExperiment(model="gemini-2.0-flash", prompt="Identify the best grasp for this object based on Feix's grasp taxonomy. For example, if you saw a piece of pencil lead, you would say: tip pinch")
-    res, answer = gemexperiment.process_sample("data/tennis/tennis1.jpg")
-    print(res)
-    print(answer)
+        return response.text
